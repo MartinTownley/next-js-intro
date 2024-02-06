97c4166bdb204d5922d741394a7352f61a54250b
martin <martinrtownley@gmail.com>
2024-02-06 11:36:16 +0000

    modify homepage and post page

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/global.css";
 
+// Component represents the current page component that needs to be rendered.
+// pageProps are the initial props that are pre-populated based on the data returned from the pages 'getInitialProps' function.
 export default function App({ Component, pageProps }) {
+  console.log("hello from _app");
   return <Component {...pageProps} />;
 }

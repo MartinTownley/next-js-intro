1ac02dd9770b474b2a02afd4ee5a3d734ea4f36f
martin <martinrtownley@gmail.com>
2024-02-08 12:51:35 +0000

    add html content to blog posts

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -59,16 +61,24 @@ export function getAllPostIds() {
   });
 }
 
-export function getPostData(id) {
+//The post page is now using the getPostData function in getStaticProps to get the post data and return it as props.
+export async function getPostData(id) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
   // use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
 
+  // use remark to convert markdown in HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
   // combine data with the ID
   return {
     id,
+    contentHtml,
     ...matterResult.data,
   };
 }
